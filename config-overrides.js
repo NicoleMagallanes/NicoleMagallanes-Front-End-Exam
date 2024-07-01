5aeebd4ade3f00365b33a658532907f4e05d681a
NicoleMagallanes <nicolemagallanes026@gmail.com>
2024-07-01 18:38:14 +0800

    chore: Update crypto-browserify dependency to version 3.12.0

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,9 @@
 const path = require("path");
 
 module.exports = function override(config) {
-  // Ensure resolve.fallback exists
   config.resolve.fallback = {
     ...config.resolve.fallback,
-    crypto: false,
+    crypto: require.resolve("crypto-browserify"),
   };
 
   return config;
